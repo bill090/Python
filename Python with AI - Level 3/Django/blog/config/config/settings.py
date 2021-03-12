2f50a0ad8e68ab2a820dd8fff666039bcc1ac407
William LIAO <william.bw.liao@gmail.com>
2021-03-12 09:21:35 -0500

    Password functions.

--- Python with AI - Level 3/Django/blog/config/config/settings.py
+++ Python with AI - Level 3/Django/blog/config/config/settings.py
@@ -123,3 +123,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+
+EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+EMAIL_FILE_PATH = str(BASE_DIR.joinpath('sent_emails'))
