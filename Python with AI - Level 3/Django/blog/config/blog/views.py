8701cef9eabb1df6c788e9e9627361f2e9754576
William LIAO <william.bw.liao@gmail.com>
2021-03-21 10:00:04 -0400

    Authentication features.

--- Python with AI - Level 3/Django/blog/config/blog/views.py
+++ Python with AI - Level 3/Django/blog/config/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .models import Post
 
 # Create your views here.
@@ -15,17 +16,27 @@ class BlogDetailView(DetailView):
     template_name = 'post_detail.html'
     context_object_name = "post"
 
-class BlogCreateView(CreateView):
-    model = Post
-    template_name = 'post_new.html'
-    fields = ['title', 'author', 'body']
-
-class BlogUpdateView(UpdateView):
+class BlogCreateView(LoginRequiredMixin, CreateView):
     model = Post
     template_name = 'post_new.html'
     fields = ['title', 'body']
 
-class BlogDeleteView(DeleteView):
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class BlogUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    template_name = 'post_new.html'
+    fields = ['title', 'body']
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
+
+class BlogDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
     template_name = 'post_delete.html'
     success_url = reverse_lazy('home')
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
